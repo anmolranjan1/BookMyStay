5ff88b36c3bb9b081d475b644c5dcd6787af87ac
Anmol Ranjan <anmolranjan1@gmail.com>
2024-09-01 02:54:02 +0530

    Database Connection Done

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,7 @@
+<?php
+require('inc/db_config.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,21 +9,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login Panel</title>
     <?php require('inc/links.php'); ?>
+    <style>
+        div.login-form {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 400px;
+        }
+    </style>
 </head>
 <body class="bg-light">
-    <div>
+    <div class="login-form text-center rounded bg-white shadow overflow-hidden">
         <form>
-            <h4>
-                ADMIN LOGIN PANEL
-            </h4>
-            <div>
+            <h4 class="bg-dark text-white py-3">ADMIN LOGIN PANEL</h4>
+            <div class="p-4">
                 <div class="mb-3">
-                    <input type="text" class="form-control shadow-none text-center" placeholder="Admin Name">
+                    <input name="admin_name" type="text" class="form-control shadow-none text-center" placeholder="Admin Name">
                 </div>
                 <div class="mb-4">
-                    <input type="password" class="form-control shadow-none" placeholder="Password">
+                    <input name="admin_pass" type="password" class="form-control shadow-none text-center" placeholder="Password">
                 </div>
-                <button type="submit">LOGIN</button>
+                <button name="login" type="submit" class="btn text-white custom-bg shadow-none">LOGIN</button>
             </div>
         </form>
     </div>
